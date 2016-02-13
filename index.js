74ecfbf21a1e3404eb11df29c47bdf2ba50ba95f
Koji Shiraishi <shiraco@gmail.com>
2016-02-13 11:34:54 +0900

    All Together Now

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    newTodo: '',
+    todos: [
+      { text: 'Add some todos' }
+    ]
   },
   methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
+    addTodo: function () {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
     }
   }
 })
